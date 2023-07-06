add635a37e2a76693ef536b7a73b156052797cfb
Eriq Augustine <eriq.augustine@gmail.com>
2023-07-06 14:00:22 -0700

    Adding some skeleton images to base graders off of.

---
+++ docker/10-python/scripts/grade.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+    # TEST
+    print("TEST -- Grade")
+
+if (__name__ == '__main__'):
+    main()
---
+++ docker/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Build all the docker images in this directory.
+# The images are build in lexicographic order.
+
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+readonly BASE_IMAGE_NAME='autograder'
+
+function main() {
+    if [[ $# -ne 0 ]]; then
+        echo "USAGE: $0"
+        exit 1
+    fi
+
+    trap exit SIGINT
+    set -e
+
+    for dockerfile in $(ls "${THIS_DIR}"/*/Dockerfile | sort) ; do
+        local buildDir=$(dirname "${dockerfile}")
+        local subImageName=$(basename "${buildDir}" | sed 's/^[0-9]\+-//')
+
+        echo "Building '${BASE_IMAGE_NAME}.${subImageName}' ..."
+        docker build --tag "${BASE_IMAGE_NAME}.${subImageName}" --file "${dockerfile}" "${buildDir}"
+    done
+
+    exit 0
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
