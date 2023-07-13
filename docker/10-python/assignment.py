71ab998a37c213b4cc1deca71256d0a919870a41
Eriq Augustine <eriq.augustine@gmail.com>
2023-07-13 14:01:05 -0700

    Updated the Python image to work with the new Python autograding interface.

---
+++ docker/10-python/assignment.py
@@ -0,0 +1,4 @@
+"""
+A dummy assignment to ensue the image builds without an actual assignment.
+Typically, the build context will be set so that assignment is available at './assignment.py'.
+"""
--- docker/10-python/scripts/grade.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    # TEST
-    print("TEST -- Grade")
-
-if (__name__ == '__main__'):
-    main()
