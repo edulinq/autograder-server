04c0145390f0887d04b67919f732e3cbaebd9f84
Eriq Augustine <eriq.augustine@gmail.com>
2023-10-08 17:19:53 -0500

    Added a utility for computing MD5 hashes of files.

---
+++ util/md5.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+    "crypto/md5"
+    "encoding/hex"
+    "fmt"
+    "io"
+    "os"
+)
+
+func MD5FileHex(path string) (string, error) {
+    file, err := os.Open(path);
+    if (err != nil) {
+        return "", fmt.Errorf("Failed to open file '%s' for MD5 hashing: '%w'.", path, err);
+    }
+    defer file.Close();
+
+    hash := md5.New()
+
+    _, err = io.Copy(hash, file);
+    if (err != nil) {
+        return "", fmt.Errorf("Failed to copy file '%s' for MD5 hashing: '%w'.", path, err);
+    }
+
+    return hex.EncodeToString(hash.Sum(nil)), nil;
+}
