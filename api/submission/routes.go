4675807cbfa45f543a0adbfe87ef10fa2bda11a9
OliverLok <43101646+OliverLok@users.noreply.github.com>
2023-12-18 21:25:26 +0000

    Added the remove submission endpoint (#39)

--- api/submission/routes.go
+++ api/submission/routes.go
@@ -13,6 +13,7 @@ var routes []*core.Route = []*core.Route{
     core.NewAPIRoute(core.NewEndpoint(`submission/fetch/submission`), HandleFetchSubmission),
     core.NewAPIRoute(core.NewEndpoint(`submission/fetch/submissions`), HandleFetchSubmissions),
     core.NewAPIRoute(core.NewEndpoint(`submission/submit`), HandleSubmit),
+    core.NewAPIRoute(core.NewEndpoint(`submission/remove`), HandleRemoveSubmission),
 };
 
 func GetRoutes() *[]*core.Route {
