67c2acce5421c69d150c8c51b274f89cebf66561
Oliver <oliver.lokhandwala@gmail.com>
2024-01-14 18:58:37 -0800

    3rd iteration

--- common/lockmanager.go
+++ common/lockmanager.go
@@ -12,42 +12,71 @@ import (
 type lockData struct {
 	timestamp time.Time;
 	mutex sync.Mutex;
+	isLocked bool;
 }
 
 var (
+	lockManagerMutex sync.Mutex;
 	lockMap sync.Map;
 	staleDuration time.Duration;
 )
 
 func init() {
-	staleDuration = time.Duration(config.STALELOCK_DURATION) * time.Second;
 	go removeStaleLocks();
 }
 
 func Lock(key string) {
 	val, _ := lockMap.LoadOrStore(key, &lockData{});
 	val.(*lockData).mutex.Lock();	
+
+	lockManagerMutex.Lock();
+	defer lockManagerMutex.Unlock()
+
 	val.(*lockData).timestamp = time.Now();
+	val.(*lockData).isLocked = true;
+
 }
 
 func Unlock(key string) error {
-	_, exists := lockMap.LoadAndDelete(key);
+	
+	val, exists := lockMap.Load(key);
+	lock := val.(*lockData);
 	if !exists {
 		log.Error().Str("key", key).Msg("Key does not exist");
-		return fmt.Errorf("Error. Key not found: %v", key);
+		return fmt.Errorf("Key not found: %v", key);
+	}
+
+	lockManagerMutex.Lock();
+	defer lockManagerMutex.Unlock();
+
+	if lock.isLocked {
+		lock.isLocked = false;
+		defer lock.mutex.Unlock();
+		lock.timestamp = time.Now();	
+	} else {
+		log.Error().Str("key", key).Msg("Tried to unlock a lock that is unlocked");
+		return fmt.Errorf("Key: %v Tried to unlock a lock that is unlocked", key);
 	}
 
 	return nil;
 }
 
 func removeStaleLocks() {
-	ticker := time.NewTicker(staleDuration);
+	ticker := time.NewTicker(time.Duration(config.STALELOCK_DURATION) * time.Second);
+	defer ticker.Stop();
 
 	for range ticker.C {
-        lockMap.Range(func(key, val interface{}) bool {
+		staleDuration := time.Duration(config.STALELOCK_DURATION) * time.Second;
+
+        lockMap.Range(func(key, val any) bool {
             lock := val.(*lockData);
-            if time.Since(lock.timestamp) > staleDuration {
-				lock.mutex.Unlock();                
+            if (time.Since(lock.timestamp) > staleDuration) && (lock.mutex.TryLock()) {
+				lock.mutex.Unlock();
+				lockManagerMutex.Lock();
+				if (time.Since(lock.timestamp) > staleDuration) && (lock.mutex.TryLock()) {
+					lockMap.Delete(key);
+				}
+				lockManagerMutex.Unlock();
             }
             return true;
         })
--- common/lockmanager_test.go
+++ common/lockmanager_test.go
@@ -2,33 +2,52 @@ package common
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
+var (
+	key = "testkey"
+	key2 = "testkey2"
+)
 
-func TestLockAcquisition(t *testing.T) {
-	
-	key := "testkey"
-	key2 := "testkey2"
-	// fmt.Println("main")
-	Lock(key);
-	
-	Lock(key2);
+
+func TestLock(t *testing.T) {
+	var wg sync.WaitGroup
+
+	Lock(key)
+	Unlock(key)
+	time.Sleep(4 * time.Second) // test remove stale lock
+
+	Lock(key)
+	Lock(key2)
 	fmt.Println("Main Thread!")
 
 	go func() {
+		wg.Add(1)
+		defer wg.Done()
 		fmt.Println("Go routine started")
 		Lock(key);
+		time.Sleep(time.Second)
 		defer Unlock(key)
-
+		go func() {
+			wg.Add(1)
+			defer wg.Done()
+			fmt.Println("Another go routine started")
+			Lock(key2)
+			defer Unlock(key2)
+			fmt.Println("Another go routine done")
+		}()
 		fmt.Println("Go routine done")
 		
 	}()
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(5000 * time.Millisecond)
 
 	go func() {
+		wg.Add(1)
+		defer wg.Done()
 		fmt.Println("2nd go routine started")
 		Lock(key2);
 		defer Unlock(key2);
@@ -36,12 +55,14 @@ func TestLockAcquisition(t *testing.T) {
 		fmt.Println("2nd go routine done")
 	}()
 
+
+
 	fmt.Println("Main Thread Almost Done!")
 	Unlock(key)
 	fmt.Println("got here")
 	Unlock(key2)
+	
+	wg.Wait()
 
-	time.Sleep(2 * time.Second);
-	Lock(key);
 	fmt.Println("Main Thread Done")
 }
