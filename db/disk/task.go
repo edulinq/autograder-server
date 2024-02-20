5397e5f66f37cd9cf17c7a0424e4a5b24925a8f8
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-02-19 22:50:10 -0800

    removed initial errors

--- db/disk/task.go
+++ db/disk/task.go
@@ -1,83 +1,83 @@
 package disk
 
 import (
-    "fmt"
-    "path/filepath"
-    "time"
+	"fmt"
+	"path/filepath"
+	"time"
 
-    "github.com/eriq-augustine/autograder/util"
+	"github.com/eriq-augustine/autograder/util"
 )
 
-const DISK_DB_TASKS_FILENAME = "tasks.json";
+const DISK_DB_TASKS_FILENAME = "tasks.json"
 
 func (this *backend) LogTaskCompletion(courseID string, taskID string, instance time.Time) error {
-    this.lock.Lock();
-    defer this.lock.Unlock();
+	this.lock.Lock()
+	defer this.lock.Unlock()
 
-    log, err := this.getTaskLog(courseID);
-    if (err != nil) {
-        return err;
-    }
+	log, err := this.getTaskLog(courseID)
+	if err != nil {
+		return err
+	}
 
-    log[taskID] = instance;
+	log[taskID] = instance
 
-    err = this.writeTaskLog(courseID, log);
-    if (err != nil) {
-        return err;
-    }
+	err = this.writeTaskLog(courseID, log)
+	if err != nil {
+		return err
+	}
 
-    return nil;
+	return nil
 }
 
 func (this *backend) GetLastTaskCompletion(courseID string, taskID string) (time.Time, error) {
-    this.lock.RLock();
-    defer this.lock.RUnlock();
+	this.lock.RLock()
+	defer this.lock.RUnlock()
 
-    log, err := this.getTaskLog(courseID);
-    if (err != nil) {
-        return time.Time{}, err;
-    }
+	log, err := this.getTaskLog(courseID)
+	if err != nil {
+		return time.Time{}, err
+	}
 
-    instance, exists := log[taskID];
-    if (!exists) {
-        return time.Time{}, nil;
-    }
+	instance, exists := log[taskID]
+	if !exists {
+		return time.Time{}, nil
+	}
 
-    return instance, nil;
+	return instance, nil
 }
 
 func (this *backend) getTasksPathFromID(courseID string) string {
-    return filepath.Join(this.getCourseDirFromID(courseID), DISK_DB_TASKS_FILENAME);
+	return filepath.Join(this.getCourseDirFromID(courseID), DISK_DB_TASKS_FILENAME)
 }
 
 func (this *backend) getTaskLog(courseID string) (map[string]time.Time, error) {
-    path := this.getTasksPathFromID(courseID);
+	path := this.getTasksPathFromID(courseID)
 
-    var log map[string]time.Time;
-    if (util.PathExists(path)) {
-        err := util.JSONFromFile(path, &log);
-        if (err != nil) {
-            return nil, fmt.Errorf("Failed to read task log '%s': '%w'.", path, err);
-        }
-    } else {
-        log = make(map[string]time.Time);
-    }
+	var log map[string]time.Time
+	if util.PathExists(path) {
+		err := util.JSONFromFile(path, &log)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to read task log '%s': '%w'.", path, err)
+		}
+	} else {
+		log = make(map[string]time.Time)
+	}
 
-    return log, nil;
+	return log, nil
 }
 
 func (this *backend) writeTaskLog(courseID string, log map[string]time.Time) error {
-    path := this.getTasksPathFromID(courseID);
+	path := this.getTasksPathFromID(courseID)
 
-    err := util.MkDir(filepath.Dir(path));
-    if (err != nil) {
-        return fmt.Errorf("Failed to create directory for task log '%s': '%w'.", path, err);
-    }
+	err := util.MkDir(filepath.Dir(path))
+	if err != nil {
+		return fmt.Errorf("Failed to create directory for task log '%s': '%w'.", path, err)
+	}
 
-    err = util.ToJSONFileIndent(log, path);
-    if (err != nil) {
-        return fmt.Errorf("Failed to write task log '%s': '%w'.", path, err);
-    }
+	err = util.ToJSONFileIndent(log, path)
+	if err != nil {
+		return fmt.Errorf("Failed to write task log '%s': '%w'.", path, err)
+	}
 
-    return nil;
+	return nil
 }
--- db/disk/user.go
+++ db/disk/user.go
@@ -1,98 +1,98 @@
 package disk
 
 import (
-    "fmt"
-    "path/filepath"
+	"fmt"
+	"path/filepath"
 
-    "github.com/eriq-augustine/autograder/model"
-    "github.com/eriq-augustine/autograder/util"
+	"github.com/eriq-augustine/autograder/model"
+	"github.com/eriq-augustine/autograder/util"
 )
 
 func (this *backend) GetUsers(course *model.Course) (map[string]*model.User, error) {
-    return this.getUsersLock(course, true);
+	return this.getUsersLock(course, true)
 }
 
 func (this *backend) getUsersLock(course *model.Course, acquireLock bool) (map[string]*model.User, error) {
-    if (acquireLock) {
-        this.lock.RLock();
-        defer this.lock.RUnlock();
-    }
+	if acquireLock {
+		this.lock.RLock()
+		defer this.lock.RUnlock()
+	}
 
-    users := make(map[string]*model.User);
+	users := make(map[string]*model.User)
 
-    path := this.getUsersPath(course);
-    if (!util.PathExists(path)) {
-        return users, nil;
-    }
+	path := this.getUsersPath(course)
+	if !util.PathExists(path) {
+		return users, nil
+	}
 
-    err := util.JSONFromFile(path, &users);
-    if (err != nil) {
-        return nil, err;
-    }
+	err := util.JSONFromFile(path, &users)
+	if err != nil {
+		return nil, err
+	}
 
-    return users, nil;
+	return users, nil
 }
 
 func (this *backend) GetUser(course *model.Course, email string) (*model.User, error) {
-    users, err := this.GetUsers(course);
-    if (err != nil) {
-        return nil, fmt.Errorf("Failed to get users when searching for '%s': '%w'.", email, err);
-    }
+	users, err := this.GetUsers(course)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get users when searching for '%s': '%w'.", email, err)
+	}
 
-    return users[email], nil;
+	return users[email], nil
 }
 
 func (this *backend) SaveUsers(course *model.Course, users map[string]*model.User) error {
-    return this.saveUsersLock(course, users, true);
+	return this.saveUsersLock(course, users, true)
 }
 
 func (this *backend) saveUsersLock(course *model.Course, newUsers map[string]*model.User, acquireLock bool) error {
-    if (acquireLock) {
-        this.lock.Lock();
-        defer this.lock.Unlock();
-    }
+	if acquireLock {
+		this.lock.Lock()
+		defer this.lock.Unlock()
+	}
 
-    users, err := this.getUsersLock(course, false);
-    if (err != nil) {
-        return fmt.Errorf("Failed to get users to merge before saving: '%w'.", err);
-    }
+	users, err := this.getUsersLock(course, false)
+	if err != nil {
+		return fmt.Errorf("Failed to get users to merge before saving: '%w'.", err)
+	}
 
-    for key, value := range newUsers {
-        users[key] = value;
-    }
+	for key, value := range newUsers {
+		users[key] = value
+	}
 
-    err = util.ToJSONFileIndent(users, this.getUsersPath(course));
-    if (err != nil) {
-        return fmt.Errorf("Unable to save user's file: '%w'.", err);
-    }
+	err = util.ToJSONFileIndent(users, this.getUsersPath(course))
+	if err != nil {
+		return fmt.Errorf("Unable to save user's file: '%w'.", err)
+	}
 
-    return nil;
+	return nil
 }
 
 func (this *backend) RemoveUser(course *model.Course, email string) error {
-    this.lock.Lock();
-    defer this.lock.Unlock();
+	this.lock.Lock()
+	defer this.lock.Unlock()
 
-    users, err := this.getUsersLock(course, false);
-    if (err != nil) {
-        return fmt.Errorf("Failed to get users when removing for '%s': '%w'.", email, err);
-    }
+	users, err := this.getUsersLock(course, false)
+	if err != nil {
+		return fmt.Errorf("Failed to get users when removing for '%s': '%w'.", email, err)
+	}
 
-    _, ok := users[email];
-    if (!ok) {
-        return nil;
-    }
+	_, ok := users[email]
+	if !ok {
+		return nil
+	}
 
-    delete(users, email);
+	delete(users, email)
 
-    err = util.ToJSONFileIndent(users, this.getUsersPath(course));
-    if (err != nil) {
-        return fmt.Errorf("Unable to save user's file: '%w'.", err);
-    }
+	err = util.ToJSONFileIndent(users, this.getUsersPath(course))
+	if err != nil {
+		return fmt.Errorf("Unable to save user's file: '%w'.", err)
+	}
 
-    return nil;
+	return nil
 }
 
 func (this *backend) getUsersPath(course *model.Course) string {
-    return filepath.Join(this.getCourseDir(course), model.USERS_FILENAME);
+	return filepath.Join(this.getCourseDir(course), model.USERS_FILENAME)
 }
