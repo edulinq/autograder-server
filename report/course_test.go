20a624776938151bd8b79abfe630bc5aec6b8a6a
Batuhan Salih <basalihcs@gmail.com>
2024-04-25 16:22:22 -0700

    got rid of the variable redeclaration

--- report/course_test.go
+++ report/course_test.go
@@ -4,7 +4,6 @@ import (
     "reflect"
     "testing"
 
-    "github.com/edulinq/autograder/common"
     "github.com/edulinq/autograder/db"
     "github.com/edulinq/autograder/util"
 )
@@ -17,9 +16,9 @@ func TestCourseReportBase(test *testing.T) {
         test.Fatalf("Failed to get course report: '%v'.", err);
     }
 
-    if (!reflect.DeepEqual(expected, report)) {
+    if (!reflect.DeepEqual(TestCourseReport, report)) {
         test.Fatalf("Report not as expected.\n--- Expected ---\n%s\n--- Actual ---\n%s\n",
-                util.MustToJSONIndent(expected), util.MustToJSONIndent(report));
+                util.MustToJSONIndent(TestCourseReport), util.MustToJSONIndent(report));
     }
 }
 
@@ -36,7 +35,7 @@ func TestCourseReportHTML(test *testing.T) {
         test.Fatalf("Failed to generate HTML for report: '%v'.", err);
     }
 
-    expectedHTML, err := expected.ToHTML();
+    expectedHTML, err := TestCourseReport.ToHTML();
     if (err != nil) {
         test.Fatalf("Failed to generate HTML for expected report: '%v'.", err);
     }
@@ -46,68 +45,3 @@ func TestCourseReportHTML(test *testing.T) {
                 expectedHTML, reportHTML);
     }
 }
-
-var expected *CourseScoringReport = &CourseScoringReport{
-    CourseName: "Course 101",
-    Assignments: []*AssignmentScoringReport{
-        &AssignmentScoringReport{
-            AssignmentName: "Homework 0",
-            NumberOfSubmissions: 1,
-            LatestSubmission: common.MustTimestampFromString("2023-10-15T21:44:33Z"),
-            Questions: []*ScoringReportQuestionStats{
-                &ScoringReportQuestionStats{
-                    QuestionName: "Q1",
-                    Min: 1,
-                    Max: 1,
-                    Median: 1,
-                    Mean: 1,
-                    StdDev: -1,
-                    MinString: "1.00",
-                    MaxString: "1.00",
-                    MedianString: "1.00",
-                    MeanString: "1.00",
-                    StdDevString: "NaN",
-                },
-                &ScoringReportQuestionStats{
-                    QuestionName: "Q2",
-                    Min: 1,
-                    Max: 1,
-                    Median: 1,
-                    Mean: 1,
-                    StdDev: -1,
-                    MinString: "1.00",
-                    MaxString: "1.00",
-                    MedianString: "1.00",
-                    MeanString: "1.00",
-                    StdDevString: "NaN",
-                },
-                &ScoringReportQuestionStats{
-                    QuestionName: "Style",
-                    Min: 0,
-                    Max: 0,
-                    Median: 0,
-                    Mean: 0,
-                    StdDev: -1,
-                    MinString: "0.00",
-                    MaxString: "0.00",
-                    MedianString: "0.00",
-                    MeanString: "0.00",
-                    StdDevString: "NaN",
-                },
-                &ScoringReportQuestionStats{
-                    QuestionName: "<Overall>",
-                    Min: 1,
-                    Max: 1,
-                    Median: 1,
-                    Mean: 1,
-                    StdDev: -1,
-                    MinString: "1.00",
-                    MaxString: "1.00",
-                    MedianString: "1.00",
-                    MeanString: "1.00",
-                    StdDevString: "NaN",
-                },
-            },
-        },
-    },
-};
