b7f393ae02105fcdf8e12b236d6ded0f67f05031
Eriq Augustine <eriq.augustine@gmail.com>
2024-07-04 19:42:11 -0400

    Added an API endpoint for creating a user token.

---
+++ internal/api/users/tokens_create_test.go
@@ -0,0 +1,37 @@
+package users
+
+import (
+	"testing"
+
+	"github.com/edulinq/autograder/internal/api/core"
+	"github.com/edulinq/autograder/internal/db"
+	"github.com/edulinq/autograder/internal/util"
+)
+
+func TestTokensCreate(test *testing.T) {
+	db.ResetForTesting()
+	defer db.ResetForTesting()
+
+	user, err := db.GetServerUser("admin@test.com", true)
+	if err != nil {
+		test.Fatalf("Failed to get initial user: '%v'.", err)
+	}
+
+	initialTokenCount := len(user.Tokens)
+
+	response := core.SendTestAPIRequest(test, core.NewEndpoint("users/tokens/create"), nil)
+	if !response.Success {
+		test.Fatalf("Response not successful: '%s'.", util.MustToJSONIndent(response))
+	}
+
+	user, err = db.GetServerUser("admin@test.com", true)
+	if err != nil {
+		test.Fatalf("Failed to get saved user: '%v'.", err)
+	}
+
+	newTokenCount := len(user.Tokens)
+
+	if newTokenCount != (initialTokenCount + 1) {
+		test.Fatalf("Incorrect token count. Expected: %d, Found: %d.", (initialTokenCount + 1), newTokenCount)
+	}
+}
