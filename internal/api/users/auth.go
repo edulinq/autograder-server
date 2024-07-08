321558c00bfa9d7994e172f432114ef55c7d29df
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-07-08 13:01:15 -0700

    Fails to auth good passwords.

--- internal/api/users/auth.go
+++ internal/api/users/auth.go
@@ -1,39 +1,44 @@
-package user
+package users
 
 import (
-    "github.com/edulinq/autograder/api/core"
+    "fmt"
+
+    "github.com/edulinq/autograder/internal/api/core"
+    "github.com/edulinq/autograder/internal/db"
+    // "github.com/edulinq/autograder/internal/util"
 )
 
 type AuthRequest struct {
-    core.APIRequestCourseUserContext
+    core.APIRequest
     core.MinServerRoleUser
 
+    TargetUser core.NonEmptyString `json:"target-email"`
     TargetPass core.NonEmptyString `json:"target-pass"`
 }
 
 type AuthResponse struct {
-    // FoundUser bool `json:"found-user"`
+    FoundUser bool `json:"found-user"`
     AuthSuccess bool `json:"auth-success"`
 }
 
 func HandleAuth(request *AuthRequest) (*AuthResponse, *core.APIError) {
     response := AuthResponse{};
 
-    // if (!request.TargetUser.Found) {
-    //     return &response, nil;
-    // }
-
-    // response.FoundUser = true;
-	user, err := db.GetServerUser(this.UserEmail, false)
-	if (err != nil) {
-        return nil, NewAuthBadRequestError("-012", this, "Cannot Get User").Err(err);
+    user, err := db.GetServerUser(string(request.TargetUser), false);
+    if (err != nil) {
+        return nil, core.NewBadRequestError("-830", &request.APIRequest, "Error getting user").Err(err);
     }
 
     if (user == nil) {
-        return nil, NewAuthBadRequestError("-013", this, "Uknown User");
+        return &response, nil;
     }
 
-    response.AuthSuccess = user.CheckPassword(string(this.UserPass));
+    fmt.Printf("User email: '%s', User pass: '%s'.\n", string(request.TargetUser), string(request.TargetPass));
+    response.FoundUser = true;
+    response.AuthSuccess, err = user.Auth(string(request.TargetPass));
+    if (err != nil) {
+        return nil, core.NewBadRequestError("-831", &request.APIRequest, "Error during Auth").Err(err);
+    }
 
     return &response, nil;
 }
