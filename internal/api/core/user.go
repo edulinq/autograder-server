2861b9ced383dd1ea543bbc0b76216a71d960bba
Lucas Ellenberger <121155755+Lucas-Ellenberger@users.noreply.github.com>
2024-09-11 12:50:28 -0700

    Added an API Endpoint for listing users. (#84)

---
+++ internal/api/core/user.go
@@ -0,0 +1,182 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/edulinq/autograder/internal/db"
+	"github.com/edulinq/autograder/internal/log"
+	"github.com/edulinq/autograder/internal/model"
+)
+
+type UserInfoType string
+
+const (
+	ServerUserInfoType = "server"
+	CourseUserInfoType = "course"
+)
+
+// This type must be embedded into any API-safe representation of a user.
+type BaseUserInfo struct {
+	Type  UserInfoType `json:"type"`
+	Email string       `json:"email"`
+	Name  string       `json:"name"`
+}
+
+// An API-safe representation of a server user.
+// Embed the BaseUserInfo and use ServerUserInfoType as the type.
+type ServerUserInfo struct {
+	BaseUserInfo
+	Role    model.ServerUserRole      `json:"role"`
+	Courses map[string]EnrollmentInfo `json:"courses"`
+}
+
+// An API-safe representation of enrollment information.
+type EnrollmentInfo struct {
+	CourseID   string               `json:"id"`
+	CourseName string               `json:"name"`
+	Role       model.CourseUserRole `json:"role"`
+}
+
+// An API-safe representation of a course user.
+// Embed the BaseUserInfo and use CourseUserInfoType as the type.
+type CourseUserInfo struct {
+	BaseUserInfo
+	Role  model.CourseUserRole `json:"role"`
+	LMSID string               `json:"lms-id"`
+}
+
+func NewServerUserInfo(user *model.ServerUser) (*ServerUserInfo, error) {
+	info := &ServerUserInfo{
+		BaseUserInfo: BaseUserInfo{
+			Type:  ServerUserInfoType,
+			Email: user.Email,
+			Name:  user.GetDisplayName(),
+		},
+		Role:    user.Role,
+		Courses: make(map[string]EnrollmentInfo, len(user.CourseInfo)),
+	}
+
+	for courseID, courseInfo := range user.CourseInfo {
+		course, err := db.GetCourse(courseID)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get course '%s' for user '%s'.", courseID, user.Email)
+		}
+
+		info.Courses[course.GetID()] = EnrollmentInfo{
+			CourseID:   course.GetID(),
+			CourseName: course.GetName(),
+			Role:       courseInfo.Role,
+		}
+	}
+
+	return info, nil
+}
+
+func MustNewServerUserInfo(user *model.ServerUser) *ServerUserInfo {
+	info, err := NewServerUserInfo(user)
+	if err != nil {
+		log.Fatal("Failed to convert server user to API info.", err, user)
+	}
+
+	return info
+}
+
+func NewServerUserInfos(users []*model.ServerUser) ([]*ServerUserInfo, error) {
+	var errs error
+
+	infos := make([]*ServerUserInfo, 0, len(users))
+	for _, user := range users {
+		info, err := NewServerUserInfo(user)
+		if err != nil {
+			err = fmt.Errorf("Failed to get server user info for user '%s': '%w'.", user.Email, err)
+			errs = errors.Join(errs, err)
+		}
+
+		infos = append(infos, info)
+	}
+
+	if errs != nil {
+		return nil, fmt.Errorf("Found errors while creating new server user infos: '%w'.", errs)
+	}
+
+	slices.SortFunc(infos, CompareServerUserInfoPointer)
+
+	return infos, nil
+}
+
+func MustNewServerUserInfos(users []*model.ServerUser) []*ServerUserInfo {
+	infos, err := NewServerUserInfos(users)
+	if err != nil {
+		log.Fatal("Failed to convert server users to API infos.", err, users)
+	}
+
+	return infos
+}
+
+func CompareServerUserInfoPointer(a *ServerUserInfo, b *ServerUserInfo) int {
+	if a == b {
+		return 0
+	}
+
+	if a == nil {
+		return 1
+	}
+
+	if b == nil {
+		return -1
+	}
+
+	return CompareServerUserInfo(*a, *b)
+}
+
+func CompareServerUserInfo(a ServerUserInfo, b ServerUserInfo) int {
+	return strings.Compare(a.Email, b.Email)
+}
+
+func NewCourseUserInfo(user *model.CourseUser) *CourseUserInfo {
+	info := &CourseUserInfo{
+		BaseUserInfo: BaseUserInfo{
+			Type:  CourseUserInfoType,
+			Email: user.Email,
+			Name:  user.GetDisplayName(),
+		},
+		Role:  user.Role,
+		LMSID: user.GetLMSID(),
+	}
+
+	return info
+}
+
+func NewCourseUserInfos(users []*model.CourseUser) []*CourseUserInfo {
+	result := make([]*CourseUserInfo, 0, len(users))
+	for _, user := range users {
+		result = append(result, NewCourseUserInfo(user))
+	}
+
+	slices.SortFunc(result, CompareCourseUserInfoPointer)
+
+	return result
+}
+
+func CompareCourseUserInfoPointer(a *CourseUserInfo, b *CourseUserInfo) int {
+	if a == b {
+		return 0
+	}
+
+	if a == nil {
+		return 1
+	}
+
+	if b == nil {
+		return -1
+	}
+
+	return CompareCourseUserInfo(*a, *b)
+}
+
+func CompareCourseUserInfo(a CourseUserInfo, b CourseUserInfo) int {
+	return strings.Compare(a.Email, b.Email)
+}
---
+++ internal/api/core/user_test.go
@@ -0,0 +1,317 @@
+package core
+
+import (
+	"reflect"
+	"slices"
+	"testing"
+
+	"github.com/edulinq/autograder/internal/db"
+	"github.com/edulinq/autograder/internal/model"
+	"github.com/edulinq/autograder/internal/util"
+)
+
+func TestNewServerUserInfos(test *testing.T) {
+	users := db.MustGetServerUsers()
+
+	testCases := []struct {
+		serverUsers []*model.ServerUser
+		expected    []*ServerUserInfo
+	}{
+		// ServerUserInfo, no course information.
+		{
+			[]*model.ServerUser{
+				users["server-user@test.edulinq.org"],
+				users["server-admin@test.edulinq.org"],
+				users["server-owner@test.edulinq.org"],
+			},
+			[]*ServerUserInfo{
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "server-admin@test.edulinq.org",
+						Name:  "server-admin",
+					},
+					Role:    model.GetServerUserRole("admin"),
+					Courses: map[string]EnrollmentInfo{},
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "server-owner@test.edulinq.org",
+						Name:  "server-owner",
+					},
+					Role:    model.GetServerUserRole("owner"),
+					Courses: map[string]EnrollmentInfo{},
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "server-user@test.edulinq.org",
+						Name:  "server-user",
+					},
+					Role:    model.GetServerUserRole("user"),
+					Courses: map[string]EnrollmentInfo{},
+				},
+			},
+		},
+
+		// ServerUserInfos, course information.
+		{
+			[]*model.ServerUser{
+				users["course-student@test.edulinq.org"],
+				users["course-admin@test.edulinq.org"],
+				users["course-grader@test.edulinq.org"],
+			},
+			[]*ServerUserInfo{
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "course-admin@test.edulinq.org",
+						Name:  "course-admin",
+					},
+					Role: model.GetServerUserRole("user"),
+					Courses: map[string]EnrollmentInfo{
+						"course-languages": {
+							CourseID:   "course-languages",
+							CourseName: "Course Using Different Languages.",
+							Role:       model.GetCourseUserRole("admin"),
+						},
+						"course-with-lms": {
+							CourseID:   "course-with-lms",
+							CourseName: "Course With LMS",
+							Role:       model.GetCourseUserRole("admin"),
+						},
+						"course-without-source": {
+							CourseID:   "course-without-source",
+							CourseName: "Course Without Source",
+							Role:       model.GetCourseUserRole("admin"),
+						},
+						"course101": {
+							CourseID:   "course101",
+							CourseName: "Course 101",
+							Role:       model.GetCourseUserRole("admin"),
+						},
+						"course101-with-zero-limit": {
+							CourseID:   "course101-with-zero-limit",
+							CourseName: "Course 101 - With Zero Limit",
+							Role:       model.GetCourseUserRole("admin"),
+						},
+					},
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "course-grader@test.edulinq.org",
+						Name:  "course-grader",
+					},
+					Role: model.GetServerUserRole("user"),
+					Courses: map[string]EnrollmentInfo{
+						"course-languages": {
+							CourseID:   "course-languages",
+							CourseName: "Course Using Different Languages.",
+							Role:       model.GetCourseUserRole("grader"),
+						},
+						"course-with-lms": {
+							CourseID:   "course-with-lms",
+							CourseName: "Course With LMS",
+							Role:       model.GetCourseUserRole("grader"),
+						},
+						"course-without-source": {
+							CourseID:   "course-without-source",
+							CourseName: "Course Without Source",
+							Role:       model.GetCourseUserRole("grader"),
+						},
+						"course101": {
+							CourseID:   "course101",
+							CourseName: "Course 101",
+							Role:       model.GetCourseUserRole("grader"),
+						},
+						"course101-with-zero-limit": {
+							CourseID:   "course101-with-zero-limit",
+							CourseName: "Course 101 - With Zero Limit",
+							Role:       model.GetCourseUserRole("grader"),
+						},
+					},
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  ServerUserInfoType,
+						Email: "course-student@test.edulinq.org",
+						Name:  "course-student",
+					},
+					Role: model.GetServerUserRole("user"),
+					Courses: map[string]EnrollmentInfo{
+						"course-languages": {
+							CourseID:   "course-languages",
+							CourseName: "Course Using Different Languages.",
+							Role:       model.GetCourseUserRole("student"),
+						},
+						"course-with-lms": {
+							CourseID:   "course-with-lms",
+							CourseName: "Course With LMS",
+							Role:       model.GetCourseUserRole("student"),
+						},
+						"course-without-source": {
+							CourseID:   "course-without-source",
+							CourseName: "Course Without Source",
+							Role:       model.GetCourseUserRole("student"),
+						},
+						"course101": {
+							CourseID:   "course101",
+							CourseName: "Course 101",
+							Role:       model.GetCourseUserRole("student"),
+						},
+						"course101-with-zero-limit": {
+							CourseID:   "course101-with-zero-limit",
+							CourseName: "Course 101 - With Zero Limit",
+							Role:       model.GetCourseUserRole("student"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for i, testCase := range testCases {
+		serverUserInfos := make([]*ServerUserInfo, 0, len(testCase.serverUsers))
+		for _, user := range testCase.serverUsers {
+			serverUserInfos = append(serverUserInfos, MustNewServerUserInfo(user))
+		}
+
+		for _, serverUserInfo := range serverUserInfos {
+			if serverUserInfo.Type != ServerUserInfoType {
+				test.Errorf("Test %d: Unexpected server user info type. Expected: '%s', actual: '%s'.",
+					i, ServerUserInfoType, serverUserInfo.Type)
+				continue
+			}
+		}
+
+		slices.SortFunc(serverUserInfos, CompareServerUserInfoPointer)
+
+		if !reflect.DeepEqual(testCase.expected, serverUserInfos) {
+			test.Errorf("Test %d: Unexpected result. Expected: '%s', actual: '%s'.",
+				i, util.MustToJSONIndent(testCase.expected), util.MustToJSONIndent(serverUserInfos))
+			continue
+		}
+	}
+}
+
+func TestNewCourseUserInfos(test *testing.T) {
+	users := db.MustGetServerUsers()
+
+	testCases := []struct {
+		serverUsers []*model.ServerUser
+		expected    []*CourseUserInfo
+	}{
+		// CourseUserInfos, enrolled in course.
+		{
+			[]*model.ServerUser{
+				users["course-student@test.edulinq.org"],
+				users["course-admin@test.edulinq.org"],
+				users["course-grader@test.edulinq.org"],
+			},
+			[]*CourseUserInfo{
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  CourseUserInfoType,
+						Email: "course-admin@test.edulinq.org",
+						Name:  "course-admin",
+					},
+					Role:  model.GetCourseUserRole("admin"),
+					LMSID: "",
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  CourseUserInfoType,
+						Email: "course-grader@test.edulinq.org",
+						Name:  "course-grader",
+					},
+					Role:  model.GetCourseUserRole("grader"),
+					LMSID: "",
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  CourseUserInfoType,
+						Email: "course-student@test.edulinq.org",
+						Name:  "course-student",
+					},
+					Role:  model.GetCourseUserRole("student"),
+					LMSID: "",
+				},
+			},
+		},
+
+		// CourseUserInfos, not enrolled, role escalation.
+		{
+			[]*model.ServerUser{
+				users["server-admin@test.edulinq.org"],
+				users["server-owner@test.edulinq.org"],
+			},
+			[]*CourseUserInfo{
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  CourseUserInfoType,
+						Email: "server-admin@test.edulinq.org",
+						Name:  "server-admin",
+					},
+					Role:  model.GetCourseUserRole("owner"),
+					LMSID: "",
+				},
+				{
+					BaseUserInfo: BaseUserInfo{
+						Type:  CourseUserInfoType,
+						Email: "server-owner@test.edulinq.org",
+						Name:  "server-owner",
+					},
+					Role:  model.GetCourseUserRole("owner"),
+					LMSID: "",
+				},
+			},
+		},
+
+		// CourseUserInfos, not enrolled, no role escalation.
+		{
+			[]*model.ServerUser{
+				users["server-user@test.edulinq.org"],
+				users["server-creator@test.edulinq.org"],
+			},
+			[]*CourseUserInfo{},
+		},
+	}
+
+	for i, testCase := range testCases {
+		courseUsers := make([]*model.CourseUser, 0, len(testCase.serverUsers))
+		for _, user := range testCase.serverUsers {
+			courseUser, err := user.ToCourseUser(db.TEST_COURSE_ID, true)
+			if err != nil {
+				test.Errorf("Test %d: Failed to convert a server user into a course user.", i)
+				continue
+			}
+
+			if courseUser != nil {
+				courseUsers = append(courseUsers, courseUser)
+			}
+		}
+
+		courseUserInfos := make([]*CourseUserInfo, 0, len(courseUsers))
+		for _, courseUser := range courseUsers {
+			courseUserInfos = append(courseUserInfos, NewCourseUserInfo(courseUser))
+		}
+
+		for _, courseUserInfo := range courseUserInfos {
+			if courseUserInfo.Type != CourseUserInfoType {
+				test.Errorf("Test %d: Unexpected course user info type. Expected: '%s', actual: '%s'.",
+					i, CourseUserInfoType, courseUserInfo.Type)
+				continue
+			}
+		}
+
+		slices.SortFunc(courseUserInfos, CompareCourseUserInfoPointer)
+
+		if !reflect.DeepEqual(testCase.expected, courseUserInfos) {
+			test.Errorf("Test %d: Unexpected result. Expected: '%s', actual: '%s'.",
+				i, util.MustToJSONIndent(testCase.expected), util.MustToJSONIndent(courseUserInfos))
+			continue
+		}
+	}
+}
