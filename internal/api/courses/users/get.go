10f30204ee66ec30a3ecb32e3336f3aa8ded938e
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-09-19 16:50:56 -0700

    Added an API endpoint for getting course users.

---
+++ internal/api/courses/users/get.go
@@ -0,0 +1,30 @@
+package users
+
+import (
+	"github.com/edulinq/autograder/internal/api/core"
+)
+
+type GetRequest struct {
+	core.APIRequestCourseUserContext
+	core.MinCourseRoleOther
+
+	TargetCourseUser core.TargetCourseUserSelfOrAdmin `json:"target-email"`
+}
+
+type GetResponse struct {
+	Found bool                 `json:"found"`
+	User  *core.CourseUserInfo `json:"user"`
+}
+
+func HandleGet(request *GetRequest) (*GetResponse, *core.APIError) {
+	response := GetResponse{}
+
+	if !request.TargetCourseUser.Found {
+		return &response, nil
+	}
+
+	response.Found = true
+	response.User = core.NewCourseUserInfo(request.TargetCourseUser.User)
+
+	return &response, nil
+}
