0aa78796d177723d3209a70cdc3a3bc75b991177
Batuhan Salih <basalih@ucsc.edu>
2024-09-21 15:28:02 -0700

    Added Dockerfile and supporting scripts

---
+++ docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+readonly THIS_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | xargs realpath)"
+readonly CMD_DIR="${THIS_DIR}/cmd"
+
+function main(){
+    local executable=$1
+    shift 1
+
+    if [ ! -d "${CMD_DIR}/${executable}" ]; then
+        echo "Could not find command: '${executable}'"
+        exit 1
+    fi
+    
+    go run ${CMD_DIR}/${executable}/main.go  "$@"
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
