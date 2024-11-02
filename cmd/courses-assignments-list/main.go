e08eb409d379ca0f8a04111835ab04d3e88c1d82
Oliver <oliver.lokhandwala@gmail.com>
2024-11-01 17:50:20 -0700

    Added a cmd to list assignments from a specific course using the new cmd infrastructure.

---
+++ cmd/courses-assignments-list/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/alecthomas/kong"
+
+	"github.com/edulinq/autograder/internal/api/core"
+	"github.com/edulinq/autograder/internal/api/courses/assignments"
+	"github.com/edulinq/autograder/internal/cmd"
+	"github.com/edulinq/autograder/internal/config"
+	"github.com/edulinq/autograder/internal/log"
+)
+
+var args struct {
+	config.ConfigArgs
+	cmd.CommonOptions
+
+	CourseID string `help:"ID of the course." arg:""`
+	Table    bool   `help:"Output data as a TSV." default:"false"`
+}
+
+func main() {
+	kong.Parse(&args,
+		kong.Description("List assignments from a specific course."),
+	)
+
+	err := config.HandleConfigArgs(args.ConfigArgs)
+	if err != nil {
+		log.Fatal("Could not load config options.", err)
+	}
+
+	request := assignments.ListRequest{
+		APIRequestCourseUserContext: core.APIRequestCourseUserContext{
+			CourseID: args.CourseID,
+		},
+	}
+
+	cmd.MustHandleCMDRequestAndExitFull(`courses/assignments/list`, request, assignments.ListResponse{}, args.CommonOptions, nil)
+}
