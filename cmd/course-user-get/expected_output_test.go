642cb5e155331041a2813fba460e204659f6bc41
Oliver <oliver.lokhandwala@gmail.com>
2024-11-04 16:12:17 -0800

    Added a cmd to get a course user's information.

---
+++ cmd/course-user-get/expected_output_test.go
@@ -0,0 +1,19 @@
+package main
+
+const USER_COURSE_FOUND = `{
+    "found": true,
+    "user": {
+        "type": "course",
+        "email": "course-student@test.edulinq.org",
+        "name": "course-student",
+        "role": "student",
+        "lms-id": "lms-course-student@test.edulinq.org"
+    }
+}
+`
+
+const USER_NOT_FOUND = `{
+    "found": false,
+    "user": null
+}
+`
