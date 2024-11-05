95fb017cf97d44abda93ec8d3b22e8d1240cbdfa
Oliver <oliver.lokhandwala@gmail.com>
2024-11-04 18:21:01 -0800

    Fixed expected test data.

--- cmd/course-user-list/expected_output_test.go
+++ cmd/course-user-list/expected_output_test.go
@@ -41,7 +41,7 @@ const EXPECTED_COURSE_USER_LIST = `{
 }
 `
 
-const EXPECTED_COURSE_USER_LIST_TABLE = `email	name	course-role	LMSID
+const EXPECTED_COURSE_USER_LIST_TABLE = `email	name	role	lms-id
 course-admin@test.edulinq.org	course-admin	admin	lms-course-admin@test.edulinq.org
 course-grader@test.edulinq.org	course-grader	grader	lms-course-grader@test.edulinq.org
 course-other@test.edulinq.org	course-other	other	lms-course-other@test.edulinq.org
