79e5e5fce85f4445090e0a90d046166e0aad057e
Oliver <oliver.lokhandwala@gmail.com>
2024-12-09 19:08:28 -0800

    Removed unneeded tests, made table conversion simpler.

--- cmd/call-endpoint/main_test.go
+++ cmd/call-endpoint/main_test.go
@@ -13,7 +13,7 @@ func TestMain(suite *testing.M) {
 	cmd.CMDServerTestingMain(suite)
 }
 
-func TestCallApiEndpointBase(test *testing.T) {
+func TestCallEndpoint(test *testing.T) {
 	testCases := []struct {
 		cmd.CommonCMDTestCase
 		endpoint   string
@@ -42,6 +42,7 @@ func TestCallApiEndpointBase(test *testing.T) {
 		},
 
 		// Custom Output Formatters.
+		// Single key with a list.
 		{
 			CommonCMDTestCase: cmd.CommonCMDTestCase{
 				ExpectedStdout: EXPECTED_COURSES_ASSIGNMENTS_TABLE,
@@ -52,6 +53,7 @@ func TestCallApiEndpointBase(test *testing.T) {
 				"--table",
 			},
 		},
+		// Single key with json.
 		{
 			CommonCMDTestCase: cmd.CommonCMDTestCase{
 				ExpectedStdout: EXPECTED_COURSES_ASSIGNMENTS_TABLE,
@@ -63,52 +65,7 @@ func TestCallApiEndpointBase(test *testing.T) {
 				"--table",
 			},
 		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_COURSES_USERS_GET_TABLE,
-			},
-			endpoint: "courses/users/get",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_FETCH_COURSE_SCORES_TABLE,
-			},
-			endpoint: "courses/assignments/submissions/fetch/course/scores",
-			parameters: []string{
-				"course-id:course101",
-				"assignment-id:hw0",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_FETCH_COURSE_ATTEMPTS_TABLE,
-			},
-			endpoint: "courses/assignments/submissions/fetch/course/attempts",
-			parameters: []string{
-				"course-id:course101",
-				"assignment-id:hw0",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_FETCH_USER_ATTEMPT_TABLE,
-			},
-			endpoint: "courses/assignments/submissions/fetch/user/attempt",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"assignment-id:hw0",
-				"target-submission:1697406256",
-				"--table",
-			},
-		},
+		// Multiple keys with a list.
 		{
 			CommonCMDTestCase: cmd.CommonCMDTestCase{
 				ExpectedStdout: EXPECTED_FETCH_USER_ATTEMPTS_TABLE,
@@ -121,88 +78,14 @@ func TestCallApiEndpointBase(test *testing.T) {
 				"--table",
 			},
 		},
+		// Multiple keys with json.
 		{
 			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_FETCH_USER_HISTORY_TABLE,
+				ExpectedStdout: EXPECTED_COURSES_USERS_GET_TABLE,
 			},
-			endpoint: "courses/assignments/submissions/fetch/user/history",
+			endpoint: "courses/users/get",
 			parameters: []string{
 				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"assignment-id:hw0",
-				"target-submission:1697406256",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_PEEK_TABLE,
-			},
-			endpoint: "courses/assignments/submissions/fetch/user/peek",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"assignment-id:hw0",
-				"target-submission:1697406256",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_SUBMISSIONS_REMOVE_TABLE,
-			},
-			endpoint: "courses/assignments/submissions/remove",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"assignment-id:hw0",
-				"target-submission:1697406256",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_LMS_USER_GET_TABLE,
-			},
-			endpoint: "lms/user/get",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"course-id:course101",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_DESCRIBE_TABLE,
-			},
-			endpoint:   "metadata/describe",
-			parameters: []string{"--table"},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_USERS_GET_TABLE,
-			},
-			endpoint: "users/get",
-			parameters: []string{
-				"target-email:course-student@test.edulinq.org",
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_SERVER_USER_LIST_TABLE,
-			},
-			endpoint: "users/list",
-			parameters: []string{
-				"--table",
-			},
-		},
-		{
-			CommonCMDTestCase: cmd.CommonCMDTestCase{
-				ExpectedStdout: EXPECTED_COURSES_USERS_LIST_TABLE,
-			},
-			endpoint: "courses/users/list",
-			parameters: []string{
 				"course-id:course101",
 				"--table",
 			},
@@ -391,41 +274,13 @@ func TestCallApiEndpointBase(test *testing.T) {
 	}
 }
 
-// Test to ensure no panic occurs when indexing into an empty list's elements during table conversion.
-func TestHandleTableConversionIndexPanic(test *testing.T) {
-	testCases := []struct {
-		response core.APIResponse
-	}{
-		// Case 0: Indexing inside of generateTableFromOneKey().
-		{
-			response: core.APIResponse{
-				Content: map[string]any{
-					"key": []any{},
-				},
-			},
-		},
-		// Case 1: Indexing inside of generateTableFromMultipleKeys().
-		{
-			response: core.APIResponse{
-				Content: map[string]any{
-					"key1": []any{},
-					"key2": []any{},
-				},
-			},
+// Test to ensure no panic occurs when converting an API response to a table.
+func TestTableConversion(test *testing.T) {
+	response := core.APIResponse{
+		Content: map[string]any{
+			"key": []any{},
 		},
 	}
 
-	for i, testCase := range testCases {
-		// Run inside a func() so defers will run independently for each test case.
-		func() {
-			defer func() {
-				value := recover()
-				if value != nil {
-					test.Errorf("Case %d: Table conversion paniced when indexing into the entries when it should have been handled.", i)
-				}
-			}()
-
-			_, _ = cmd.ConvertApiResponseToTable(testCase.response)
-		}()
-	}
+	_, _ = cmd.ConvertApiResponseToTable(response)
 }
--- internal/cmd/request.go
+++ internal/cmd/request.go
@@ -114,46 +114,43 @@ func PrintCMDResponseFull(request any, response core.APIResponse, responseType a
 	}
 
 	output := ""
-	if successfulConversion && customPrintFunc != nil {
+	if successfulConversion {
 		output = customPrintOutput
-	} else if responseType == nil {
-		output = util.MustToJSONIndent(response.Content)
 	} else {
-		responseContent := reflect.New(reflect.TypeOf(responseType)).Interface()
-		util.MustJSONFromString(util.MustToJSON(response.Content), &responseContent)
-		output = util.MustToJSONIndent(responseContent)
+		if responseType != nil {
+			responseContent := reflect.New(reflect.TypeOf(responseType)).Interface()
+			util.MustJSONFromString(util.MustToJSON(response.Content), &responseContent)
+			output = util.MustToJSONIndent(responseContent)
+		} else {
+			output = util.MustToJSONIndent(response.Content)
+		}
 	}
 
 	fmt.Println(output)
 }
 
-// Attempt to convert an API response to a TSV table.
-// Return ("", false) if there are issues converting the API response to a table
-// or (customTable.String(), true) if the response got sucessfully converted.
+// Attempt to convert an API response into a TSV table.
+// If the response content has multiple keys or a single map, it creates a single row table.
+// If it has one key with a list, the values in each element becomes a row of the table.
+// Return the table and true on a successful conversion or false if there is an issue converting.
 func ConvertApiResponseToTable(response core.APIResponse) (string, bool) {
 	responseContent, ok := response.Content.(map[string]any)
 	if !ok {
 		return "", false
 	}
 
-	// Case 1: Multiple keys in the response content.
-	if len(responseContent) > 1 {
-		return generateTableFromMultipleKeys(responseContent)
-	}
-
-	// Case 2: A single key in the response content.
-	// If the key's value is a map, treat it as a single row table.
-	// If the key's value is a list, treat each element in the list as a row.
-	if len(responseContent) == 1 {
+	if len(responseContent) == 0 {
+		return "", false
+	} else if len(responseContent) > 1 {
+		return convertEntriesToTable([]any{responseContent})
+	} else {
 		var responseContentKey string
 		for key := range responseContent {
 			responseContentKey = key
 		}
 
-		content := responseContent[responseContentKey]
-
 		var entries []any
-		switch value := content.(type) {
+		switch value := responseContent[responseContentKey].(type) {
 		case map[string]any:
 			entries = []any{value}
 		case []any:
@@ -162,60 +159,18 @@ func ConvertApiResponseToTable(response core.APIResponse) (string, bool) {
 			return "", false
 		}
 
-		return generateTableFromOneKey(entries)
+		return convertEntriesToTable(entries)
 	}
-
-	return "", false
 }
 
-func generateTableFromMultipleKeys(responseContent map[string]any) (string, bool) {
-	var responseContentKeys []string
-	for key := range responseContent {
-		responseContentKeys = append(responseContentKeys, key)
-	}
-
-	sort.Strings(responseContentKeys)
-
-	var entries []any
-	for _, key := range responseContentKeys {
-		entries = append(entries, responseContent[key])
-	}
-
-	var customTable strings.Builder
-	customTable.WriteString(strings.Join(responseContentKeys, "\t") + "\n")
-
-	// Turn the entry into a row of the table.
-	for i, entry := range entries {
-		row := ""
-		switch value := entry.(type) {
-		case map[string]any:
-			row = util.MustToJSON(value)
-		case []any:
-			if len(value) == 0 {
-				return "", false
-			}
-
-			row = util.MustToJSON(value[0])
-		default:
-			row = fmt.Sprintf("%v", entry)
-		}
-
-		customTable.WriteString(row)
-
-		if i < (len(entries) - 1) {
-			customTable.WriteString("\t")
-		}
-	}
-
-	return customTable.String(), true
-}
-
-func generateTableFromOneKey(entries []any) (string, bool) {
+// Attempt to convert a slice of entries into a TSV table.
+// Headers are taken from the keys of the first entry.
+// Rows are the value's of each entry in the slice.
+func convertEntriesToTable(entries []any) (string, bool) {
 	if len(entries) == 0 {
 		return "", false
 	}
 
-	// Use the first entry to create the headers of the table.
 	firstEntry, ok := entries[0].(map[string]any)
 	if !ok {
 		return "", false
@@ -238,19 +193,19 @@ func generateTableFromOneKey(entries []any) (string, bool) {
 			return "", false
 		}
 
-		customTable.WriteString("\n")
-
 		var row []string
 		for _, key := range headers {
 			switch value := entryMap[key].(type) {
 			case map[string]any:
+				row = append(row, util.MustToJSON(value))
+			case []any:
 				row = append(row, util.MustToJSON(value))
 			default:
 				row = append(row, fmt.Sprintf("%v", value))
 			}
 		}
 
-		customTable.WriteString(strings.Join(row, "\t"))
+		customTable.WriteString("\n" + strings.Join(row, "\t"))
 	}
 
 	return customTable.String(), true
