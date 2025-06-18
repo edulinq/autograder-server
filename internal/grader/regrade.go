0bb6262f5c5cce128f5ab88e0f1a542c2f83e54c
Lucas Ellenberger <lmellenb@ucsc.edu>
2025-06-18 12:44:59 -0700

    (wip) Working on the regrade integration into a job.

--- internal/grader/regrade.go
+++ internal/grader/regrade.go
@@ -1,8 +1,15 @@
 package grader
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/edulinq/autograder/internal/config"
 	"github.com/edulinq/autograder/internal/db"
 	"github.com/edulinq/autograder/internal/jobmanager"
+	"github.com/edulinq/autograder/internal/log"
+	"github.com/edulinq/autograder/internal/model"
+	"github.com/edulinq/autograder/internal/timestamp"
 )
 
 type RegradeOptions struct {
@@ -10,7 +17,7 @@ type RegradeOptions struct {
 	GradeOptions
 
 	// The raw references of users to regrade.
-	RawReferences []CourseUserReferences `json:"target-users" required:""`
+	RawReferences []model.CourseUserReference `json:"target-users" required:""`
 
 	// If true, do not swap the context to the background context when running.
 	// By default (when this is false), the context will be swapped to the background context when !WaitForCompletion.
@@ -24,39 +31,46 @@ type RegradeOptions struct {
 }
 
 func Regrade(options RegradeOptions) (map[string]*model.SubmissionHistoryItem, int, map[string]string, error) {
-	reference, err := ParseCourseUserReferences(options.RawReferences)
+	reference, err := model.ParseCourseUserReferences(options.RawReferences)
 	if err != nil {
 		return nil, 0, nil, fmt.Errorf("Failed to parse course user references: '%w'.", err)
 	}
 
-	fullUsers := model.ResolveCourseUserEmails(db.GetCourseUsers(options.Assignment.GetCourse().GetID(), reference))
+	courseUsers, err := db.GetCourseUsers(options.Assignment.GetCourse())
+	if err != nil {
+		return nil, 0, nil, fmt.Errorf("Failed to get course users: '%w'.", err)
+	}
+
+	fullUsers := model.ResolveCourseUserEmails(courseUsers, reference)
 
 	if !options.RetainOriginalContext && !options.WaitForCompletion {
 		options.Context = context.Background()
 	}
 
-	job := jobmanager.Job[string, *model.IndividualAnalysis]{
+	// TODO: We may want to return the proxy time!
+	var proxyTime timestamp.Timestamp = 0
+	if options.ProxyTime == nil {
+		proxyTime = timestamp.Now()
+	} else {
+		proxyTime = *options.ProxyTime
+	}
+
+	lockKey := fmt.Sprintf("regrade-course-%s", options.Assignment.GetCourse().GetID())
+
+	job := jobmanager.Job[string, *model.SubmissionHistoryItem]{
 		JobOptions:              &options.JobOptions,
-		LockKey:                 fmt.Sprintf("regrade-course-%s", options.Assignment.GetCourse().GetID()),
+		LockKey:                 lockKey,
 		PoolSize:                config.REGRADE_COURSE_POOL_SIZE.Get(),
 		ReturnIncompleteResults: !options.WaitForCompletion,
 		WorkItems:               fullUsers,
-		// TODO: Does this need to be modified to not include nil results? Guess: probably
-		RetrieveFunc: db.GetRecentSubmissionSurvey(options.Assignment, reference),
-		StoreFunc:    db.StoreIndividualAnalysis,
-		RemoveFunc:   db.RemoveIndividualAnalysis,
-		WorkFunc: func(fullSubmissionID string) (*model.IndividualAnalysis, error) {
-			return computeSingleIndividualAnalysis(options, fullSubmissionID, true)
+		RetrieveFunc: func(resolvedEmails []string) (map[string]*model.SubmissionHistoryItem, error) {
+			return retrieveRegradedSubmissions(options.Assignment, proxyTime, resolvedEmails)
 		},
-		WorkItemKeyFunc: func(fullSubmissionID string) string {
-			return fmt.Sprintf("analysis-individual-%s", fullSubmissionID)
+		WorkFunc: func(email string) (*model.SubmissionHistoryItem, error) {
+			return computeSingleRegrade(options, email)
 		},
-		OnComplete: func(result *jobmanager.JobOutput[string, *model.IndividualAnalysis]) {
-			if result == nil {
-				return
-			}
-
-			collectIndividualStats(fullSubmissionIDs, result.RunTime, options.InitiatorEmail)
+		WorkItemKeyFunc: func(email string) string {
+			return fmt.Sprintf("%s-%s", lockKey, email)
 		},
 	}
 
@@ -82,4 +96,41 @@ func Regrade(options RegradeOptions) (map[string]*model.SubmissionHistoryItem, i
 	}
 
 	return output.ResultItems, len(output.RemainingItems), workErrors, nil
+}
+
+func computeSingleRegrade(options RegradeOptions, email string) (*model.SubmissionHistoryItem, error) {
+	return nil, nil
+}
+
+func retrieveRegradedSubmissions(assignment *model.Assignment, proxyTime timestamp.Timestamp, emails []string) (map[string]*model.SubmissionHistoryItem, error) {
+	emailSet := make(map[string]any, len(emails))
+	for _, email := range emails {
+		emailSet[email] = nil
+	}
+
+	reference := &model.ParsedCourseUserReference{
+		Emails: emailSet,
+	}
+
+	results, err := db.GetRecentSubmissionSurvey(assignment, reference)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get recent submissions from db: '%w'.", err)
+	}
+
+	finalResults := make(map[string]*model.SubmissionHistoryItem, len(results))
+
+	for email, result := range results {
+		if result == nil {
+			continue
+		}
+
+		// The submission was made before the regrade threshold.
+		if result.GradingStartTime < proxyTime {
+			continue
+		}
+
+		finalResults[email] = result
+	}
+
+	return finalResults, nil
 }
