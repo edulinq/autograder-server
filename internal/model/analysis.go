53982ff4d639bd6f8d23c87f076b13dd19d369ab
anvichip <anvikohli13@gmail.com>
2025-07-16 10:00:28 +0000

    Improved commenting

--- internal/model/analysis.go
+++ internal/model/analysis.go
@@ -28,9 +28,9 @@ type AssignmentAnalysisOptions struct {
 	TemplateFiles   []*util.FileSpec      `json:"template-files,omitempty,omitzero"`
 	TemplateFileOps []*util.FileOperation `json:"template-file-ops,omitempty,omitzero"`
 
-	// EngineOptions include the parameters for the engines that will be used for the analysis.
+	// EngineOptions includes the parameters for the engines that will be used for the analysis.
 	// Current implementation includes minTokens for JPlag.
-	// Current expected structure: {"jplag": {"minTokens": YourValue}} minTokens accepts a int value
+	// Current expected structure: {"jplag": {"minTokens": YourValue}} minTokens accepts int type.
 	EngineOptions map[string]any `json:"engine-options,omitempty,omitzero"`
 }
 
