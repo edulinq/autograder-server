aa34e453ebb05dbb4fbf4ca8f81c6823c0317657
Eriq Augustine <eriq.augustine@gmail.com>
2026-02-08 10:04:00 -0800

    Replaced uses of cmd/describe-api-endpoints with cmd/call-endpoint.

--- scripts/update_api_description.sh
+++ scripts/update_api_description.sh
@@ -4,6 +4,7 @@ readonly THIS_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | xargs realpa
 readonly ROOT_DIR="${THIS_DIR}/.."
 readonly OUT_DIR="${ROOT_DIR}/resources"
 readonly OUT_FILE="${OUT_DIR}/api.json"
+readonly TEMP_FILE="/tmp/autograder-api.json"
 
 function main() {
     if [[ $# -ne 0 ]] ; then
@@ -17,10 +18,16 @@ function main() {
 
     mkdir -p "${OUT_DIR}"
 
-    go run cmd/describe-api-endpoints/main.go > "${OUT_FILE}"
+    go run cmd/call-endpoint/main.go metadata/describe > "${TEMP_FILE}"
     if [ $? -ne 0 ]; then
         echo "Failed to update ${OUT_FILE}."
         return 1
+    fi
+
+    mv "${TEMP_FILE}" "${OUT_FILE}"
+    if [ $? -ne 0 ]; then
+        echo "Failed to move result from temp file (${TEMP_FILE}) to output file (${OUT_FILE})."
+        return 2
     fi
 
     echo "API description successfully updated in ${OUT_FILE}."
